cd5da94f88eba2e4d147b09e509f242df296f763
George Havranek <ghavranek@merrillmount.com>
2020-08-18 11:14:51 -0400

    iss57

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-
+#iss56
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-
+#iss57
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
