514e0ba4c06456062fda60ac167bc6e68b81fd4c
George Havranek <ghavranek@merrillmount.com>
2020-08-18 10:54:09 -0400

    Just testing branching

--- app.py
+++ app.py
@@ -43,6 +43,6 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         }
     )
 ])
-
+# test branch
 if __name__ == '__main__':
     app.run_server(debug=True,host='0.0.0.0')
