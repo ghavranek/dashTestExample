6322127151c6d82bf78b510cd630258b0a92cbe2
George Havranek <ghavranek@merrillmount.com>
2020-08-18 11:01:22 -0400

    hotfix

--- app.py
+++ app.py
@@ -44,5 +44,6 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     )
 ])
 # test branch
+# hotfix
 if __name__ == '__main__':
     app.run_server(debug=True,host='0.0.0.0')
